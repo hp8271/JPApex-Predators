b2af2b5fb512a36232cfe7c8bf33a0d1d8bb9511
Harsh Pranay <harshpranay8271@gmail.com>
2025-03-30 22:33:29 +0530

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Harsh Pranay on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
