451be5ec34d4316b84d538ae59e72e4d73f39120
Harsh Pranay <harshpranay8271@gmail.com>
2025-03-31 17:56:51 +0530

    1. opening of dino detail page on click form the lists

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Harsh Pranay on 31/03/25.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
